15fd662b7ceb5ac9d35a699b8a1919b89242d43b
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2025-11-26 10:18:02 +0530

    Create chat.js

---
+++ api/chat.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const message =
+    (req.query && req.query.message) ||
+    (req.body && req.body.message) ||
+    "no message received";
+
+  res.status(200).json({
+    reply: Echo from TatvaBot: ${message},
+  });
+}
