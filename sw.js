f4281e0130fdf27ab372f179644ed996954ff689
myselfneerajnaiyar-hash <myselfneerajnaiyar@gmail.com>
2025-12-27 22:43:16 +0530

    Add service worker for PWA

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
+const CACHE_NAME = "tatvabot-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/chat.js",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
